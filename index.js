e6d41009bc855c425fc95c6906b5b0bad1d61fce
Kennedy-munene <kennedymunene309@gmail.com>
2022-12-06 14:50:14 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+
+//Dodge to the Left
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left =parseInt(leftNumbers, 10)
+if (left > 0) {
+    dodger.style.left = `${left-1}px`;
+
+}   
+}
+//Dodge to the Right
+function moveDodgerRight (){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left =parseInt(leftNumbers, 10)
+if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+
+}   
+}
+
+document.addEventListener("keydown", function (e) {
+    console.log(e);
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+    else if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+});
